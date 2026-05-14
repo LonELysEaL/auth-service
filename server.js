d5530ae213288fd66fbec1be2a05e7819ad60e0a
LonELysEaL <136128326+LonELysEaL@users.noreply.github.com>
2026-05-14 21:56:20 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const SECRET = "MY_SECRET_KEY";
+
+const users = [
+  {
+    id: 1,
+    username: "admin",
+    password: "1234",
+    max_sessions: 2
+  }
+];
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  const user = users.find(
+    u => u.username === username && u.password === password
+  );
+
+  if (!user) {
+    return res.status(401).json({ error: "wrong login" });
+  }
+
+  const token = jwt.sign(
+    {
+      userId: user.id,
+      username: user.username,
+      max_sessions: user.max_sessions
+    },
+    SECRET,
+    { expiresIn: "7d" }
+  );
+
+  res.json({ token });
+});
+
+app.get("/verify", (req, res) => {
+  const token = req.headers.authorization;
+
+  if (!token) return res.json({ valid: false });
+
+  try {
+    const decoded = jwt.verify(token, SECRET);
+
+    res.json({
+      valid: true,
+      user: decoded
+    });
+  } catch {
+    res.json({ valid: false });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Auth service running");
+});
